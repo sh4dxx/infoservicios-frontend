cd2ae5cfddab9c3869d48e9dcf89a17073df62c4
FelipeH <felipeandrshh@gmail.com>
2025-08-27 17:02:20 -0400

    Cambio a backend de produccion

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'https://65274bed917d673fd76d8c5b.mockapi.io/api/v1/'
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://infoservicios-backend.onrender.com'
+  // baseURL: 'http://localhost:3001'
 })
 
 export default api
